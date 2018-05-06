c33631dbb495509e936951af842b9a48c97bccd3
Csaba Ivancza <csaba.ivancza@gmail.com>
2018-05-06 13:33:53 +0200

    checkpoint

--- my_first_sim/scripts/main.py
+++ my_first_sim/scripts/main.py
@@ -19,7 +19,7 @@ sys.path.insert(0, package_path + '/../')
 
 pprint(sys.path)
 
-from scripts.mybot import MyBot
+from mybot import MyBot
 
 
 def get_sensors_data(range_list):
@@ -36,6 +36,24 @@ def get_avg(data):
     return sum(data) / len(data)
 
 
+def speeding_up_if_possible(my_bot, turning_value):
+    tmp = None
+    i = 0
+    for item in my_bot.last_n_steps:
+        if tmp != item:
+            tmp = item
+            i = 0
+        else:
+            i += 1
+
+    if i > 5:
+        print('speeding up')
+        my_bot.last_n_steps = turning_value[1]
+        turning_value = (1.5, turning_value[1])
+
+    return turning_value
+
+
 if __name__ == '__main__':
 
     with Morse() as simu:
@@ -43,7 +61,7 @@ if __name__ == '__main__':
         my_bot = MyBot(simu)
         allowed_distance = 1
         laser_window = 1.5
-        critical_distance = 0.5
+        critical_distance = 0.8
 
         while True:
             range_list = my_bot.get_rangelist()
@@ -55,49 +73,39 @@ if __name__ == '__main__':
             rd_avg = get_avg(right_sensors)
             md_avg = get_avg(middle_sensors)
 
-            objects_near_to_bot = [x < allowed_distance for x in [ld_avg, md_avg, rd_avg]]
-
             total_left = left_sensors[0]
             middle = middle_sensors[1]
             total_right = right_sensors[len(right_sensors)-1]
 
-            print(ld_avg, md_avg, rd_avg)
-
-            general_value = (0 - (ld_avg - rd_avg))
+            general_value = 0 - (ld_avg - rd_avg)
+            turning_value = (0.5, general_value)
 
             if ld_avg < laser_window or md_avg < laser_window or rd_avg < laser_window:
                 my_bot.slow_down()
 
-            if md_avg < allowed_distance:
+                any_of_them_is_zero = (c < critical_distance for c in range_list)
 
-                # there is a trap, try to find a way out
-                if total_left < critical_distance and total_right < critical_distance and middle < critical_distance:
-                    my_bot.last_n_steps = -4
-                    my_bot.turn_sharp().wait()
+                if md_avg < allowed_distance:
 
-                print('something in my sight, turning somewhere')
-                w = general_value * 6
-                my_bot.last_n_steps = w
-                turning_value = (0.1, w)
-            else:
-                print('correcting my way')
-                my_bot.last_n_steps = general_value
-                turning_value = (0.5, general_value)
-
-            ls = my_bot.last_n_steps
-            tmp = None
-            i = 0
-            for item in ls:
-                if tmp != item:
-                    tmp = item
-                    i = 0
+                    # there is a trap, try to find a way out
+                    if total_left < critical_distance and total_right < critical_distance and middle < critical_distance:
+                        my_bot.last_n_steps = -4
+                        my_bot.turn_sharp().wait()
+                    elif any(any_of_them_is_zero):
+                        print('stucked...')
+                        my_bot.turn_sharp().wait()
+                    else:
+                        w = 0 - (total_left - total_right)
+                        print('something in my sight, turning somewhere %s' % w)
+                        my_bot.last_n_steps = w
+                        turning_value = (0.1, w)
                 else:
-                    i += 1
+                    print('correcting my way')
+                    my_bot.last_n_steps = general_value
+                    turning_value = (0.5, general_value)
 
-            if i > 5:
-                print('speeding up')
-                my_bot.last_n_steps = turning_value[1]
-                turning_value = (1.5, turning_value[1])
+            my_bot.last_n_steps = general_value
+            turning_value = speeding_up_if_possible(my_bot, turning_value)
 
             my_bot.move(*turning_value)
 
